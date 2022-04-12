f6272b772e007c63638c39643b27abb56e302133
Igor Guedes <48987652+igorcguedes@users.noreply.github.com>
2022-04-12 00:50:12 -0300

    Update post.sh

--- post.sh
+++ post.sh
@@ -3,10 +3,26 @@
 sudo xbps-install -Su void-repo-multilib void-repo-multilib-nonfree void-repo-nonfree
 
 # softwares
-sudo xbps-install -Su adwaita-icon-theme bash-completion bc chromium rofi engrampa firefox git htop i3 libglvnd-32bit libreoffice lutris kitty neofetch feh nvidia nvidia-libs-32bit obs thunar pulseaudio pulsemixer remmina steam thunderbird transmission-gtk unzip vba-m virtualbox-ose vulkan-loader-32bit wget wine-32bit wine-gecko wine-mono winetricks xclip flameshot xorg youtube-dl zathura-pdf-poppler
+sudo xbps-install -Su adwaita-icon-theme bash-completion bc chromium rofi engrampa firefox git htop  libglvnd-32bit lutris kitty neofetch feh nvidia nvidia-libs-32bit thunar pulseaudio pulsemixer remmina steam thunderbird unzip vba-m virtualbox-ose vulkan-loader-32bit wget wine-32bit wine-gecko wine-mono winetricks xclip flameshot xorg youtube-dl zathura-pdf-poppler
 
 # fonts 
 sudo ln -s /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/
 
 # reboot
 sudo reboot
+
+
+
+
+
+#pdf-reader=
+#file-manager=thunar & ranger
+#tar&zips=Xarchiver
+#themes-manager=Lxappareance
+#pcinfo=htop
+#soundcontrol=pavucontrol
+#media-player=mpv
+#sound-player=mpd & npcmpp
+#screenshot=flameshot
+#app-launcher=rofi
+#office=open-office.appimg
